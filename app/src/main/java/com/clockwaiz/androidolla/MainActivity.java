52b04eb5a9044350597f8a5f733f443955fda69a
Clockwaiz <clockwaiz@gmail.com>
2017-08-16 23:49:49 +0800

    Add hellotext Add Button function to update hello to olla

--- app/src/main/java/com/clockwaiz/androidolla/MainActivity.java
+++ app/src/main/java/com/clockwaiz/androidolla/MainActivity.java
@@ -1,13 +1,31 @@
 package com.clockwaiz.androidolla;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtText)
+    TextView txtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+    }
+
+
+    @OnClick(R.id.btnOlla)
+    public void onClickBtnOlla(){
+        txtText.setText(R.string.olla_message);
     }
 }
